95e1e10cc04da52069be48c03897a223a454c0bd
programming <e.babyle206@gmail.com>
2024-03-11 21:18:35 +0530

    add files

---
+++ blog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render,redirect
+from pytube import*
+# Create your views here.
+def youtube(request):
+    if request.method == 'POST':
+        link=request.POST['link']
+        video=YouTube(link)
+        stream=video.streams.get_lowest_resolution()
+        stream.download()
+        return render(request,'blog/index.html')
+    return render(request,'blog/index.html')
